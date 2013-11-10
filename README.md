0d71d6d03837e0f120583b81d3f9118cb3869ae6
Feross Aboukhadijeh <feross@feross.org>
2013-11-09 17:59:50 -0800

    Pulled code out of `chrome-app-socket` into its own module

---
+++ README.md
@@ -0,0 +1,43 @@
+chrome-dgram
+==========
+### Use the Node `dgram` API in Chrome Apps
+
+This module lets you use the Node.js [dgram](http://nodejs.org/api/dgram.html) (UDP) API in [Chrome Packaged Apps](http://developer.chrome.com/apps/about_apps.html).
+
+Instead of learning the quirks of Chrome's `chrome.socket` API for networking in Chrome Apps just **use the higher-level node API you're familiar with**. Then, compile your code with [browserify](https://github.com/substack/node-browserify) and you're all set!
+
+This module is used by [webtorrent](https://github.com/feross/webtorrent).
+
+## Installation
+
+`npm install chrome-app-socket`
+
+## Usage
+
+Use node's `dgram` API, including all parameter list shorthands and variations.
+
+Example UDP client/bind:
+
+```js
+var dgram = require('chrome-app-socket').dgram
+
+var sock = dgram.createSocket('udp4')
+
+sock.send('beep', 0, 'beep'.length, 1337, '127.0.0.1')
+
+sock.on('message', function (data, rInfo) {
+  console.log('Got data from ' + rInfo.address + ':' + rInfo.port)
+  console.log(data)
+})
+
+```
+
+See nodejs.org for full API documentation: [dgram](http://nodejs.org/api/dgram.html)
+
+## Contributing
+
+To run tests, use `npm test`. The tests will run TCP and UDP servers and launch a few different Chrome Packaged Apps with browserified client code. The tests currently require Chrome Canary on Mac. If you're on Windows or Linux, feel free to send a pull request to fix this limitation.
+
+## MIT License
+
+Copyright (c) [Feross Aboukhadijeh](http://feross.org) & John Hiesey
---
+++ index.js
@@ -0,0 +1,401 @@
+/* global chrome */
+
+/**
+ * UDP / Datagram Sockets
+ * ======================
+ *
+ * Datagram sockets are available through require('chrome-dgram').
+ */
+
+exports.Socket = Socket
+
+var bops = require('bops')
+var EventEmitter = require('events').EventEmitter
+var util = require('util')
+
+var BIND_STATE_UNBOUND = 0
+var BIND_STATE_BINDING = 1
+var BIND_STATE_BOUND = 2
+
+/**
+ * Convert given data to a Uint8Array. The underlying ArrayBuffer is
+ * guaranteed to be the same size as the Uint8Array.
+ *
+ * @param  {Uint8Array|ArrayBuffer|string} data
+ * @return {Uint8Array}      [description]
+ */
+function toBuffer (data) {
+  if (bops.is(data)) {
+    if (data.length === data.buffer.length) {
+      return data
+    } else {
+      // If data is a Uint8Array (TypedArrayView) AND its underlying ArrayBuffer
+      // has a different size (larger) then we create a new Uint8Array and
+      // underlying ArrayBuffer that are the exact same size. This is necessary
+      // because Chrome's `sendTo` consumes the underlying ArrayBuffer.
+      var newBuf = bops.create(data.length)
+      bops.copy(data, newBuf, 0, 0, data.length)
+      return newBuf
+    }
+  } else if (typeof data === 'string') {
+    return bops.from(data)
+  } else if (data instanceof ArrayBuffer) {
+    return new Uint8Array(data)
+  } else {
+    throw new Error('Cannot convert data to ArrayBuffer type')
+  }
+}
+
+/**
+ * dgram.createSocket(type, [callback])
+ *
+ * Creates a datagram Socket of the specified types. Valid types are `udp4`
+ * and `udp6`.
+ *
+ * Takes an optional callback which is added as a listener for message events.
+ *
+ * Call socket.bind if you want to receive datagrams. socket.bind() will bind
+ * to the "all interfaces" address on a random port (it does the right thing
+ * for both udp4 and udp6 sockets). You can then retrieve the address and port
+ * with socket.address().address and socket.address().port.
+ *
+ * @param  {string} type       Either 'udp4' or 'udp6'
+ * @param  {function} listener Attached as a listener to message events.
+ *                             Optional
+ * @return {Socket}            Socket object
+ */
+exports.createSocket = function (type, listener) {
+  return new Socket(type, listener)
+}
+
+util.inherits(Socket, EventEmitter)
+
+/**
+ * Class: dgram.Socket
+ *
+ * The dgram Socket class encapsulates the datagram functionality. It should
+ * be created via `dgram.createSocket(type, [callback])`.
+ *
+ * Event: 'message'
+ *   - msg Uint8Array object. The message
+ *   - rinfo Object. Remote address information
+ *   Emitted when a new datagram is available on a socket. msg is a Buffer and
+ *   rinfo is an object with the sender's address information and the number
+ *   of bytes in the datagram.
+ *
+ * Event: 'listening'
+ *   Emitted when a socket starts listening for datagrams. This happens as soon
+ *   as UDP sockets are created.
+ *
+ * Event: 'close'
+ *   Emitted when a socket is closed with close(). No new message events will
+ *   be emitted on this socket.
+ *
+ * Event: 'error'
+ *   - exception Error object
+ *   Emitted when an error occurs.
+ */
+function Socket (type, listener) {
+  var self = this
+  EventEmitter.call(self)
+
+  if (type !== 'udp4')
+    throw new Error('Bad socket type specified. Valid types are: udp4')
+
+  if (typeof listener === 'function')
+    self.on('message', listener)
+
+  self._destroyed = false
+  self._bindState = BIND_STATE_UNBOUND
+}
+
+/**
+ * socket.bind(port, [address], [callback])
+ *
+ * For UDP sockets, listen for datagrams on a named port and optional address.
+ * If address is not specified, the OS will try to listen on all addresses.
+ * After binding is done, a "listening" event is emitted and the callback(if
+ * specified) is called. Specifying both a "listening" event listener and
+ * callback is not harmful but not very useful.
+ *
+ * A bound datagram socket keeps the node process running to receive
+ * datagrams.
+ *
+ * If binding fails, an "error" event is generated. In rare case (e.g. binding
+ * a closed socket), an Error may be thrown by this method.
+ *
+ * @param {number} port
+ * @param {string} address Optional
+ * @param {function} callback Function with no parameters, Optional. Callback
+ *                            when binding is done.
+ */
+Socket.prototype.bind = function (port, address, callback) {
+  var self = this
+  if (typeof address === 'function') {
+    callback = address
+    address = undefined
+  }
+
+  if (!address)
+    address = '0.0.0.0'
+
+  if (self._bindState !== BIND_STATE_UNBOUND)
+    throw new Error('Socket is already bound')
+
+  self._bindState = BIND_STATE_BINDING
+
+  if (typeof callback === 'function')
+    self.once('listening', callback)
+
+  chrome.socket.create('udp', {}, function (createInfo) {
+    self.id = createInfo.socketId
+
+    chrome.socket.bind(self.id, address, port, function (result) {
+      if (result < 0) {
+        self.emit('error', new Error('Socket ' + self.id + ' failed to bind'))
+        return
+      }
+      chrome.socket.getInfo(self.id, function (result) {
+        if (!result.localPort) {
+          self.emit(new Error('Cannot get local port for Socket ' + self.id))
+          return
+        }
+
+        self._port = result.localPort
+        self._address = result.localAddress
+
+        self._bindState = BIND_STATE_BOUND
+        self.emit('listening')
+
+        self._recvLoop()
+      })
+    })
+  })
+}
+
+/**
+ * Internal function to receive new messages and emit `message` events.
+ */
+Socket.prototype._recvLoop = function() {
+  var self = this
+
+  chrome.socket.recvFrom(self.id, function (recvFromInfo) {
+    if (recvFromInfo.resultCode === 0) {
+      self.close()
+
+    } else if (recvFromInfo.resultCode < 0) {
+      self.emit('error', new Error('Socket ' + self.id + ' recvFrom error ' +
+          recvFromInfo.resultCode))
+
+    } else {
+      self.emit('message', new Uint8Array(recvFromInfo.data), recvFromInfo)
+      self._recvLoop()
+    }
+  })
+}
+
+/**
+ * socket.send(buf, offset, length, port, address, [callback])
+ *
+ * For UDP sockets, the destination port and IP address must be
+ * specified. A string may be supplied for the address parameter, and it will
+ * be resolved with DNS. An optional callback may be specified to detect any
+ * DNS errors and when buf may be re-used. Note that DNS lookups will delay
+ * the time that a send takes place, at least until the next tick. The only
+ * way to know for sure that a send has taken place is to use the callback.
+ *
+ * If the socket has not been previously bound with a call to bind, it's
+ * assigned a random port number and bound to the "all interfaces" address
+ * (0.0.0.0 for udp4 sockets, ::0 for udp6 sockets).
+ *
+ * @param {ArrayBuffer|TypedArray|string} buf Message to be sent
+ * @param {number} offset Offset in the buffer where the message starts.
+ * @param {number} length Number of bytes in the message.
+ * @param {number} port destination port
+ * @param {string} address destination IP
+ * @param {function} callback Callback when message is done being delivered.
+ *                            Optional.
+ */
+// Socket.prototype.send = function (buf, host, port, cb) {
+Socket.prototype.send = function (buffer,
+                                  offset,
+                                  length,
+                                  port,
+                                  address,
+                                  callback) {
+
+  var self = this
+  buffer = toBuffer(buffer).buffer
+  if (!callback) callback = function () {}
+
+  if (offset !== 0)
+    throw new Error('Non-zero offset not supported yet')
+
+  if (self._bindState === BIND_STATE_UNBOUND)
+    self.bind(0)
+
+  // If the socket hasn't been bound yet, push the outbound packet onto the
+  // send queue and send after binding is complete.
+  if (self._bindState !== BIND_STATE_BOUND) {
+    // If the send queue hasn't been initialized yet, do it, and install an
+    // event handler that flishes the send queue after binding is done.
+    if (!self._sendQueue) {
+      self._sendQueue = []
+      self.once('listening', function () {
+        // Flush the send queue.
+        for (var i = 0; i < self._sendQueue.length; i++) {
+          self.send.apply(self, self._sendQueue[i])
+        }
+        self._sendQueue = undefined
+      })
+    }
+    self._sendQueue.push([buffer, offset, length, port, address, callback])
+    return
+  }
+
+  chrome.socket.sendTo(self.id, buffer, address, port, function (writeInfo) {
+    if (writeInfo.bytesWritten < 0) {
+      var ex = new Error('Socket ' + self.id + ' send eror ' + writeInfo.bytesWritten)
+      callback(ex)
+      self.emit('error', ex)
+    } else {
+      callback(null)
+    }
+  })
+}
+
+/**
+ * Close the underlying socket and stop listening for data on it.
+ */
+Socket.prototype.close = function () {
+  var self = this
+  if (!self._destroyed)
+    return
+
+  chrome.socket.destroy(self.id)
+  self._destroyed = true
+
+  self.emit('close')
+}
+
+/**
+ * Returns an object containing the address information for a socket. For UDP
+ * sockets, this object will contain address, family and port.
+ *
+ * @return {Object} information
+ */
+Socket.prototype.address = function () {
+  var self = this
+  return {
+    address: self._address,
+    port: self._port,
+    family: 'IPv4'
+  }
+}
+
+Socket.prototype.setBroadcast = function (flag) {
+  // No chrome.socket equivalent
+}
+
+Socket.prototype.setTTL = function (ttl) {
+  // No chrome.socket equivalent
+}
+
+// NOTE: Multicast code is untested. Pull requests accepted for bug fixes and to
+// add tests!
+
+/**
+ * Sets the IP_MULTICAST_TTL socket option. TTL stands for "Time to Live," but
+ * in this context it specifies the number of IP hops that a packet is allowed
+ * to go through, specifically for multicast traffic. Each router or gateway
+ * that forwards a packet decrements the TTL. If the TTL is decremented to 0
+ * by a router, it will not be forwarded.
+ *
+ * The argument to setMulticastTTL() is a number of hops between 0 and 255.
+ * The default on most systems is 1.
+ *
+ * NOTE: The Chrome version of this function is async, whereas the node
+ * version is sync. Keep this in mind.
+ *
+ * @param {number} ttl
+ * @param {function} callback CHROME-SPECIFIC: Called when the configuration
+ *                            operation is done.
+ */
+Socket.prototype.setMulticastTTL = function (ttl, callback) {
+  var self = this
+  if (!callback) callback = function () {}
+  chrome.socket.setMulticastTimeToLive(self.id, ttl, callback)
+}
+
+/**
+ * Sets or clears the IP_MULTICAST_LOOP socket option. When this option is
+ * set, multicast packets will also be received on the local interface.
+ *
+ * NOTE: The Chrome version of this function is async, whereas the node
+ * version is sync. Keep this in mind.
+ *
+ * @param {boolean} flag
+ * @param {function} callback CHROME-SPECIFIC: Called when the configuration
+ *                            operation is done.
+ */
+Socket.prototype.setMulticastLoopback = function (flag, callback) {
+  var self = this
+  if (!callback) callback = function () {}
+  chrome.socket.setMulticastLoopbackMode(self.id, flag, callback)
+}
+
+/**
+ * Tells the kernel to join a multicast group with IP_ADD_MEMBERSHIP socket
+ * option.
+ *
+ * If multicastInterface is not specified, the OS will try to add membership
+ * to all valid interfaces.
+ *
+ * NOTE: The Chrome version of this function is async, whereas the node
+ * version is sync. Keep this in mind.
+ *
+ * @param {string} multicastAddress
+ * @param {string} [multicastInterface] Optional
+ * @param {function} callback CHROME-SPECIFIC: Called when the configuration
+ *                            operation is done.
+ */
+Socket.prototype.addMembership = function (multicastAddress,
+                                           multicastInterface,
+                                           callback) {
+  var self = this
+  if (!callback) callback = function () {}
+  chrome.socket.joinGroup(self.id, multicastAddress, callback)
+}
+
+/**
+ * Opposite of addMembership - tells the kernel to leave a multicast group
+ * with IP_DROP_MEMBERSHIP socket option. This is automatically called by the
+ * kernel when the socket is closed or process terminates, so most apps will
+ * never need to call this.
+ *
+ * NOTE: The Chrome version of this function is async, whereas the node
+ * version is sync. Keep this in mind.
+ *
+ * If multicastInterface is not specified, the OS will try to drop membership
+ * to all valid interfaces.
+ *
+ * @param  {[type]} multicastAddress
+ * @param  {[type]} multicastInterface Optional
+ * @param {function} callback CHROME-SPECIFIC: Called when the configuration
+ *                            operation is done.
+ */
+Socket.prototype.dropMembership = function (multicastAddress,
+                                            multicastInterface,
+                                            callback) {
+  var self = this
+  if (!callback) callback = function () {}
+  chrome.socket.leaveGroup(self.id, multicastAddress, callback)
+}
+
+Socket.prototype.unref = function () {
+  // No chrome.socket equivalent
+}
+
+Socket.prototype.ref = function () {
+  // No chrome.socket equivalent
+}
